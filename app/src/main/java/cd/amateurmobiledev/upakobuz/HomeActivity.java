5b1bfb65d069aa0d1b404a1a82fa91986eed9d29
josh muleshi <jmuleshi2@gmail.com>
2021-02-11 13:26:11 +0000

    right menu on the toolbar fixed and well done

--- app/src/main/java/cd/amateurmobiledev/upakobuz/HomeActivity.java
+++ app/src/main/java/cd/amateurmobiledev/upakobuz/HomeActivity.java
@@ -1,14 +1,17 @@
 package cd.amateurmobiledev.upakobuz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.Menu;
-import android.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
-    public Toolbar toolbar;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +26,26 @@ public class HomeActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbarmenu,menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.share:
+                Toast.makeText(getApplicationContext(),"Share Clicked",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.about:
+                Toast.makeText(getApplicationContext(),"About us Clicked",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.setting:
+                Toast.makeText(getApplicationContext(),"Setting Clicked",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.search:
+                Toast.makeText(getApplicationContext(),"Search Clicked",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
