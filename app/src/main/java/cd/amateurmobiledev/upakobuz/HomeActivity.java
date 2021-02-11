cc98d964d0415f68a969c6e42c5109edee5239cd
josh muleshi <jmuleshi2@gmail.com>
2021-02-11 13:04:09 +0000

    add data on a toolbar

--- app/src/main/java/cd/amateurmobiledev/upakobuz/HomeActivity.java
+++ app/src/main/java/cd/amateurmobiledev/upakobuz/HomeActivity.java
@@ -3,12 +3,25 @@ package cd.amateurmobiledev.upakobuz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
+
+    public Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbarmenu,menu);
+        return true;
     }
 }
