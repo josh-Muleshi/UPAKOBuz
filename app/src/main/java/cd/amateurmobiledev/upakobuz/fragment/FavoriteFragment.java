fa855ccd956779c1c07bc44e195783f30d0aceee
josh muleshi <jmuleshi2@gmail.com>
2021-02-14 18:01:28 +0000

    add all fragments classes files

---
+++ app/src/main/java/cd/amateurmobiledev/upakobuz/fragment/FavoriteFragment.java
@@ -0,0 +1,23 @@
+package cd.amateurmobiledev.upakobuz.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import cd.amateurmobiledev.upakobuz.R;
+
+public class FavoriteFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.favorite_fragment,container,true);
+
+        return rootView;
+    }
+}
