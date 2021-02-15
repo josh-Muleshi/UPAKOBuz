47de938af568a73f789fd90d7f6a7e6a0e0dfb50
josh muleshi <jmuleshi2@gmail.com>
2021-02-15 09:24:43 +0000

    navigation items add Listener and action

--- app/src/main/java/cd/amateurmobiledev/upakobuz/HomeActivity.java
+++ app/src/main/java/cd/amateurmobiledev/upakobuz/HomeActivity.java
@@ -14,7 +14,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+import cd.amateurmobiledev.upakobuz.fragment.FavoriteFragment;
+import cd.amateurmobiledev.upakobuz.fragment.HomeFragment;
+import cd.amateurmobiledev.upakobuz.fragment.MessageFragment;
+import cd.amateurmobiledev.upakobuz.fragment.NotificationFragment;
+import cd.amateurmobiledev.upakobuz.fragment.ProfileFragment;
+import cd.amateurmobiledev.upakobuz.fragment.ShareFragment;
+
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawerLayout;
 
@@ -30,8 +39,11 @@ public class HomeActivity extends AppCompatActivity {
 
         drawerLayout = findViewById(R.id.drawer_layout);
 
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 
         toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.things));
@@ -41,10 +53,42 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new HomeFragment()).commit();
+                break;
+            case R.id.nav_favorite:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new FavoriteFragment()).commit();
+                break;
+            case R.id.nav_massage:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MessageFragment()).commit();
+                break;
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProfileFragment()).commit();
+                break;
+            case R.id.nav_notification:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new NotificationFragment()).commit();
+                break;
+            case R.id.nav_share:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ShareFragment()).commit();
+                break;
+        }
+        return true;
+    }
+
+    @Override
     public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
-        }else {
+        } else {
             super.onBackPressed();
         }
     }
@@ -59,8 +103,8 @@ public class HomeActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.notification){
-            Toast.makeText(this, "notification clicked",Toast.LENGTH_SHORT).show();
+        if (id == R.id.notification) {
+            Toast.makeText(this, "notification clicked", Toast.LENGTH_SHORT).show();
             return true;
         }
         return super.onOptionsItemSelected(item);
